29d1cf41d274cc2ad61efc60b2dc60938c6f877f
Matheus Silva <ms25022003@gmail.com>
2025-11-26 16:08:58 -0300

    feat(weather): new weather attributes for atmospheric conditions

--- back/src/modules/weather/weather.schema.ts
+++ back/src/modules/weather/weather.schema.ts
@@ -23,6 +23,14 @@ export class Weather {
   wind_gusts: number;
   @Prop()
   apparent_temperature: number;
+  @Prop()
+  precipitation: number;
+  @Prop()
+  cloud_cover: number;
+  @Prop()
+  relative_humidity: number;
+  @Prop()
+  weather_code: number;
 }
 
 export const WeatherSchema = SchemaFactory.createForClass(Weather);
