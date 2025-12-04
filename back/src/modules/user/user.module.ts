550ec2228f22c4b7add40899b1e945a19d653061
Matheus Silva <ms25022003@gmail.com>
2025-12-04 18:03:22 -0300

    fix(user): add UserSeederService to providers

--- back/src/modules/user/user.module.ts
+++ back/src/modules/user/user.module.ts
@@ -3,12 +3,13 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user.controller';
+import { UserSeederService } from './seeder/user-seeder.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [UserService],
+  providers: [UserService, UserSeederService],
   exports: [UserService],
   controllers: [UserController],
 })
